0fd489c8b31e9c542646f0ba6e10af0b997c6cd3
NgoTruongVu05 <ngotruongvu05@gmail.com>
2024-11-30 21:46:35 +0700

    Add files via upload

---
+++ admin/admin/asset/js/modal.js
@@ -0,0 +1,60 @@
+const deleteButtons = document.querySelectorAll(".tool-menu-fix-delete .row-element .delete");
+const modal = document.querySelector('.modal');
+const behindMenuModal = document.querySelector('.behindMenuModal');
+const behindMenuAlert = document.querySelector('.behindMenuAlert');
+const cancelButton = document.querySelector('.modal .choiceModal .cancel');
+const confirmButton = document.querySelector('.modal .choiceModal .confirm');
+
+let elementToDelete = null;
+
+deleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener('click', (event) => {
+        modal.classList.toggle("show", true);
+        behindMenuModal.style.display = 'block';
+        elementToDelete = event.target.closest('.grid-row');
+        console.log(elementToDelete);
+
+        if (cancelButton) {
+            cancelButton.addEventListener('click', () => {
+                modal.classList.toggle("show", false);
+                behindMenuModal.style.display = 'none';
+                elementToDelete = null;
+            })
+        }
+        if (confirmButton) {
+            confirmButton.addEventListener('click', () => {
+                if (elementToDelete) {
+                    elementToDelete.remove();
+                    modal.classList.toggle("show", false);
+                    behindMenuModal.style.display = 'none';
+                    elementToDelete = null;
+
+                    //Alert
+                    behindMenuAlert.style.display = 'block';
+                    const customAlert = document.getElementById('customAlert');
+                    customAlert.style.display = 'block';
+                    const closeButton = document.getElementById('alertCloseBtn');
+                    closeButton.addEventListener('click', () => {
+                        customAlert.style.display = 'none';
+                        behindMenuAlert.style.display = 'none';
+                    });
+                }
+            })
+        }
+    })
+})
+
+behindMenuModal.addEventListener('click', () => {
+    modal.classList.toggle("show", false);
+    behindMenuModal.style.display = 'none';
+})
+
+behindMenuAlert.addEventListener('click', () => {
+    modal.classList.toggle("show", false);
+    behindMenuAlert.style.display = 'none';
+    customAlert.style.display = 'none';
+})
+
+
+
+
