ad3f1689cdc749e1231ad3ec7ad3a15e7433dc20
UrName2412 <thienduong24122005@gmail.com>
2024-12-20 23:44:20 +0700

    new file:   js/user.js 	modified:   nguoidung/user.html

---
+++ js/user.js
@@ -0,0 +1,21 @@
+const iconCartSpan = document.querySelector(".cart-icon span");
+let productCart = [];
+
+const loadFromLocalStorage = () => {
+    const storedCart = localStorage.getItem('cart');
+    cart = storedCart ? JSON.parse(storedCart) : [];
+
+    // Update cart icon with the total quantity on page load
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
+
+    if (totalQuantity < 99) {
+        iconCartSpan.innerText = totalQuantity;
+    } else {
+        iconCartSpan.innerText = '99+';
+    }
+};
+
+loadFromLocalStorage();
