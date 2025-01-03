1919ffebf1796da08f23e44eb14306580254983a
NgoTruongVu05 <ngotruongvu05@gmail.com>
2025-01-03 22:39:16 +0700

    modified:   admin/asset/css/style.css 	modified:   admin/asset/js/inputDataProduct.js 	modified:   admin/asset/js/inputDataUser.js 	modified:   admin/data/JSON/nguoidung.json 	modified:   admin/data/JSON/sanpham.json 	modified:   admin/page/nguoidung.html 	modified:   admin/page/sanpham.html

--- admin/asset/js/inputDataProduct.js
+++ admin/asset/js/inputDataProduct.js
@@ -1,56 +1,46 @@
-
-
 var productsAPI = '../data/JSON/sanpham.json';
 
+let products = [];
+var listProductsBlock = document.querySelector('#dataProducts');
+
 function start() {
-    getProducts(function (products) {
-        renderProducts(products);
+    getProducts().then(() => {
+        renderProducts();
     });
 }
 
-start();
 
 //function
-function getProducts(callback) {
-    fetch(productsAPI)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(callback);
+function getProducts() {
+    return fetch(productsAPI)
+        .then(response => response.json())
+        .then(data => {
+            products = data;
+        });
 }
 
-function renderProducts(products) {
-    // Nội dung trong bảng
-    var listProductsBlock = document.querySelector('#dataProducts');
-    products.forEach(function (product) {
-        // Tạo phần tử mới
-        var newProduct = document.createElement('div');
-        newProduct.className = 'grid-row-product';
-        newProduct.innerHTML = `
-            <textarea placeholder="Nhập nội dung..." readonly>${product.id}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${product.name}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${product.author}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${product.category}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${product.nxb}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${product.total}</textarea>
-            <div class="input-picture">
-                <input type="file" name="picture" class="picture" placeholder="Chọn ảnh" onchange="displayFileName()" style="display: none;">
-                <textarea placeholder="Nhập nội dung..." class="file-name" readonly>${product.picture}</textarea>
-            </div>
-            <div class="tool">
-                <button type="button" class="fix">
-                    <i class="fas fa-wrench"></i>
-                </button>
-                <button type="button" class="delete">
-                    <i class="fas fa-trash"></i>
-                </button>
-            </div>
-        `;
+function renderProducts() {
+    activeProducts(listProductsBlock);
+}
 
-        // Thêm phần tử vào DOM
-        listProductsBlock.appendChild(newProduct);
-    });
-    
+function fixButtons(){
+    var fixButtons = document.querySelectorAll('.fix');
+    fixButtons.forEach((fixButton) => {
+        fixButton.addEventListener('click', (event) => {
+            var gridRow = event.target.closest('.grid-row-product');
+            if (gridRow.classList.contains('fixed')) {
+                fixButton.innerHTML = `<i class="fas fa-wrench"></i>`;
+            } else {
+                fixButton.innerHTML = `<i class="fas fa-check"></i>`;
+            }
+            gridRow.classList.toggle('fixed');
+            fixButton.classList.toggle('fixed');
+
+        })
+    })
+}
+
+function deleteButtons(){
     var deleteButtons = document.querySelectorAll('.delete');
     const stringModal = 'Bạn có chắc muốn xóa sản phẩm không?';
     const stringAlert = 'Đã xóa.';
@@ -58,45 +48,203 @@ function renderProducts(products) {
         deleteButton.addEventListener('click', (event) => {
             var confirm = openModal(stringModal, stringAlert).then((result) => {
                 if (result) {
-                    if (deleteButton){
-                        var gridRow = event.target.closest('.grid-row');
-                        console.log(gridRow);
-                        //Chưa xong
+                    if (deleteButton) {
+                        var gridRow = event.target.closest('.grid-row-product');
+                        gridRow.remove();
+                        let id = gridRow.querySelector('.grid-row-product textarea[placeholder="Nhập id..."]').value;
+                        let index = products.findIndex(product => product.id == id);
+                        products[index].isDeleted = "true";
                     }
                 }
             });
-            
         });
     })
+}
+function deleteButtonsAllProducts(){
+    var deleteButtons = document.querySelectorAll('.delete');
+    const stringModal = 'Bạn có chắc muốn xóa sản phẩm không?';
+    const stringAlert = 'Đã xóa.';
+    deleteButtons.forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal, stringAlert).then((result) => {
+                if (result) {
+                    if (deleteButton) {
+                        var gridRow = event.target.closest('.grid-row-product');
+                        let id = gridRow.querySelector('.grid-row-product textarea[placeholder="Nhập id..."]').value;
+                        let index = products.findIndex(product => product.id == id);
+                        products[index].isDeleted = "true";
+                        allProducts(listProductsBlock);
+                    }
+                }
+            });
+        });
+    })
+}
 
-    var fixButtons = document.querySelectorAll('.fix');
-    fixButtons.forEach((fixButton) => {
-        fixButton.addEventListener('click', (event) => {
-            var gridRowProduct = event.target.closest('.grid-row-product');
-            var textAreas = gridRowProduct.querySelectorAll('textarea');
-            if (gridRowProduct.classList.contains('fixed')) {
-                textAreas.forEach((textArea) => {
-                    textArea.readOnly = true;
-                    textArea.classList.toggle('fixed');
-                })
-                fixButton.innerHTML = `<i class="fas fa-wrench"></i>`;
-            } else {
-                textAreas.forEach((textArea) => {
-                    textArea.readOnly = false;
-                    textArea.classList.toggle('fixed');
-                })
-                fixButton.innerHTML = `<i class="fas fa-check"></i>`;
-            }
-            gridRowProduct.classList.toggle('fixed');
-            fixButton.classList.toggle('fixed');
+function restoreButtons(){
+    var restoreButtons = document.querySelectorAll('.restore');
+    const stringModal = 'Bạn có chắc muốn khôi phục sản phẩm không?';
+    const stringAlert = 'Đã khôi phục.';
+    restoreButtons.forEach((restoreButton,index) => {
+        restoreButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal,stringAlert).then((result) => {
+                if (result) {
+                    if (restoreButton) {
+                        var gridRow = event.target.closest('.grid-row-product');
+                        gridRow.remove();
+                        let id = gridRow.querySelector('.grid-row-product textarea[placeholder="Nhập id..."]').value;
+                        let index = products.findIndex(product => product.id == id);
+                        products[index].isDeleted = "false";
+                    }
+                }
+            })
+        })
+    })
+}
+function restoreButtonsAllProducts(){
+    var restoreButtons = document.querySelectorAll('.restore');
+    const stringModal = 'Bạn có chắc muốn khôi phục sản phẩm không?';
+    const stringAlert = 'Đã khôi phục.';
+    restoreButtons.forEach((restoreButton,index) => {
+        restoreButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal,stringAlert).then((result) => {
+                if (result) {
+                    if (restoreButton) {
+                        var gridRow = event.target.closest('.grid-row-product');
+                        let id = gridRow.querySelector('.grid-row-product textarea[placeholder="Nhập id..."]').value;
+                        let index = products.findIndex(product => product.id == id);
+                        products[index].isDeleted = "false";
+                        allProducts(listProductsBlock);
+                    }
+                }
+            })
         })
     })
 }
 
+function productFilter(){
+    const productFilter = document.getElementById('productFilter');
+    products.forEach(function (product) {
+        if (productFilter.value == "activeProducts") activeProducts(listProductsBlock);
+        else if (productFilter.value == "deletedProducts") deletedProducts(listProductsBlock);
+        else if (productFilter.value == "allProducts") allProducts(listProductsBlock);
+    });
+}
 
-// function displayFileName() {
-//     const fileInput = document.querySelector('.picture');
-//     console.log(fileInput);
-//     const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : "Chưa chọn ảnh";
-//     document.que('file-name').textContent = fileName;
-// }
+function activeProducts(listProductsBlock){
+    listProductsBlock.innerHTML = '';
+    products.forEach(function (product) {
+        if (product.isDeleted == "false"){
+            var newProduct = document.createElement('div');
+            newProduct.className = 'grid-row-product';
+            newProduct.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${product.id}</textarea>
+                <textarea placeholder="Nhập tên sản phẩm..." readonly>${product.name}</textarea>
+                <textarea placeholder="Nhập tên tác giả..." readonly>${product.author}</textarea>
+                <textarea placeholder="chọn thể loại..." readonly>${product.category}</textarea>
+                <textarea placeholder="Nhập nhà xuất bản..." readonly>${product.nxb}</textarea>
+                <textarea placeholder="Nhập giá tiền..." readonly>${product.total}</textarea>
+                <div class="input-picture">
+                    <input type="file" name="picture" class="picture" placeholder="Chọn ảnh" onchange="displayFileName()" style="display: none;">
+                    <textarea placeholder="Nhập nội dung..." class="file-name" readonly>${product.picture}</textarea>
+                </div>
+                <div class="tool">
+                    <button type="button" class="fix">
+                        <i class="fas fa-wrench"></i>
+                    </button>
+                    <button type="button" class="delete">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </div>
+            `;
+            listProductsBlock.appendChild(newProduct);
+        }  
+    });
+    fixButtons();
+    deleteButtons();
+}
+
+function allProducts(listProductsBlock){
+    listProductsBlock.innerHTML = '';
+    products.forEach(function (product) {
+        var newProduct = document.createElement('div');
+        newProduct.className = 'grid-row-product';
+        if (product.isDeleted == "true"){
+            newProduct.classList.add('banned');
+            newProduct.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${product.id}</textarea>
+                <textarea placeholder="Nhập tên sản phẩm..." readonly>${product.name}</textarea>
+                <textarea placeholder="Nhập tên tác giả..." readonly>${product.author}</textarea>
+                <textarea placeholder="chọn thể loại..." readonly>${product.category}</textarea>
+                <textarea placeholder="Nhập nhà xuất bản..." readonly>${product.nxb}</textarea>
+                <textarea placeholder="Nhập giá tiền..." readonly>${product.total}</textarea>
+                <div class="input-picture">
+                    <input type="file" name="picture" class="picture" placeholder="Chọn ảnh" onchange="displayFileName()" style="display: none;">
+                    <textarea placeholder="Nhập nội dung..." class="file-name" readonly>${product.picture}</textarea>
+                </div>
+                <div class="tool">
+                    <button type="button" class="restore">
+                        <i class="fas fa-trash-restore"></i>
+                    </button>
+                </div>
+            `;
+        } else if (product.isDeleted == "false"){
+            newProduct.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${product.id}</textarea>
+                <textarea placeholder="Nhập tên sản phẩm..." readonly>${product.name}</textarea>
+                <textarea placeholder="Nhập tên tác giả..." readonly>${product.author}</textarea>
+                <textarea placeholder="chọn thể loại..." readonly>${product.category}</textarea>
+                <textarea placeholder="Nhập nhà xuất bản..." readonly>${product.nxb}</textarea>
+                <textarea placeholder="Nhập giá tiền..." readonly>${product.total}</textarea>
+                <div class="input-picture">
+                    <input type="file" name="picture" class="picture" placeholder="Chọn ảnh" onchange="displayFileName()" style="display: none;">
+                    <textarea placeholder="Nhập nội dung..." class="file-name" readonly>${product.picture}</textarea>
+                </div>
+                <div class="tool">
+                    <button type="button" class="fix">
+                        <i class="fas fa-wrench"></i>
+                    </button>
+                    <button type="button" class="delete">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </div>
+            `;
+        }
+        listProductsBlock.appendChild(newProduct);  
+    });
+    fixButtons();
+    deleteButtonsAllProducts();
+    restoreButtonsAllProducts();
+}
+
+function deletedProducts(listProductsBlock){
+    listProductsBlock.innerHTML = '';
+    products.forEach(function (product) {
+        if (product.isDeleted == "true"){
+            var newProduct = document.createElement('div');
+            newProduct.className = 'grid-row-product';
+            newProduct.classList.add('banned');
+            newProduct.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${product.id}</textarea>
+                <textarea placeholder="Nhập tên sản phẩm..." readonly>${product.name}</textarea>
+                <textarea placeholder="Nhập tên tác giả..." readonly>${product.author}</textarea>
+                <textarea placeholder="chọn thể loại..." readonly>${product.category}</textarea>
+                <textarea placeholder="Nhập nhà xuất bản..." readonly>${product.nxb}</textarea>
+                <textarea placeholder="Nhập giá tiền..." readonly>${product.total}</textarea>
+                <div class="input-picture">
+                    <input type="file" name="picture" class="picture" placeholder="Chọn ảnh" onchange="displayFileName()" style="display: none;">
+                    <textarea placeholder="Nhập nội dung..." class="file-name" readonly>${product.picture}</textarea>
+                </div>
+                <div class="tool">
+                    <button type="button" class="restore">
+                        <i class="fas fa-trash-restore"></i>
+                    </button>
+                </div>
+            `;
+            listProductsBlock.appendChild(newProduct);
+        }  
+    });
+    restoreButtons();
+}
+
+start();
--- admin/asset/js/inputDataUser.js
+++ admin/asset/js/inputDataUser.js
@@ -1,51 +1,46 @@
-
-
 var usersAPI = '../data/JSON/nguoidung.json';
 
+let users = [];
+var listUsersBlock = document.querySelector('#dataUsers');
+
 function start() {
-    getUsers(function (users) {
-        renderUsers(users);
+    getUsers().then(() => {
+        renderUsers();
     });
 }
 
-start();
 
 //function
-function getUsers(callback) {
-    fetch(usersAPI)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(callback);
+function getUsers() {
+    return fetch(usersAPI)
+        .then(response => response.json())
+        .then(data => {
+            users = data;
+        });
 }
 
-function renderUsers(users) {
+function renderUsers() {
     // Nội dung trong bảng
-    var listUsersBlock = document.querySelector('#dataUsers');
-    users.forEach(function (user) {
-        // Tạo phần tử mới
-        var newUser = document.createElement('div');
-        newUser.className = 'grid-row';
-        newUser.innerHTML = `
-            <textarea placeholder="Nhập id..." readonly>${user.id}</textarea>
-            <textarea placeholder="Nhập tên người dùng..." readonly>${user.username}</textarea>
-            <textarea placeholder="Nhập số điện thoại..." readonly>${user.phone}</textarea>
-            <textarea placeholder="Nhập email..." readonly>${user.email}</textarea>
-            <textarea placeholder="Nhập nội dung..." readonly>${user.address}</textarea>
-            <div class="tool">
-                <button type="button" class="fix">
-                    <i class="fas fa-wrench"></i>
-                </button>
-                <button type="button" class="delete">
-                    <i class="fas fa-ban"></i>
-                </button>
-            </div>
-        `;
+    activeUsers(listUsersBlock);
+}
 
-        // Thêm phần tử vào DOM
-        listUsersBlock.appendChild(newUser);
-    });
+function fixButtons(){
+    var fixButtons = document.querySelectorAll('.fix');
+    fixButtons.forEach((fixButton) => {
+        fixButton.addEventListener('click', (event) => {
+            var gridRow = event.target.closest('.grid-row');
+            if (gridRow.classList.contains('fixed')) {
+                fixButton.innerHTML = `<i class="fas fa-wrench"></i>`;
+            } else {
+                fixButton.innerHTML = `<i class="fas fa-check"></i>`;
+            }
+            gridRow.classList.toggle('fixed');
+            fixButton.classList.toggle('fixed');
+        })
+    })
+}
 
+function banButtons(){
     var banButtons = document.querySelectorAll('.delete');
     const stringModal = 'Bạn có chắc muốn khóa người dùng không?';
     const stringAlert = 'Đã khóa.';
@@ -55,37 +50,182 @@ function renderUsers(users) {
                 if (result) {
                     if (banButton) {
                         var gridRow = event.target.closest('.grid-row');
-                        console.log(gridRow);
-                        //Chưa xong
+                        gridRow.remove();
+                        let id = gridRow.querySelector('.grid-row textarea[placeholder="Nhập id..."]').value;
+                        let index = users.findIndex(user => user.id == id);
+                        users[index].isBanned = "true";
                     }
                 }
             });
-
         });
     })
+}
+function banButtonsAllUsers(){
+    var banButtons = document.querySelectorAll('.delete');
+    const stringModal = 'Bạn có chắc muốn khóa người dùng không?';
+    const stringAlert = 'Đã khóa.';
+    banButtons.forEach((banButton, index) => {
+        banButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal, stringAlert).then((result) => {
+                if (result) {
+                    if (banButton) {
+                        var gridRow = event.target.closest('.grid-row');
+                        let id = gridRow.querySelector('.grid-row textarea[placeholder="Nhập id..."]').value;
+                        let index = users.findIndex(user => user.id == id);
+                        users[index].isBanned = "true";
+                        allUsers(listUsersBlock);
+                    }
+                }
+            });
+        });
+    })
+}
 
-    var fixButtons = document.querySelectorAll('.fix');
-    fixButtons.forEach((fixButton) => {
-        fixButton.addEventListener('click', (event) => {
-            var gridRow = event.target.closest('.grid-row');
-            var textAreas = gridRow.querySelectorAll('textarea');
-            if (gridRow.classList.contains('fixed')) {
-                textAreas.forEach((textArea) => {
-                    textArea.readOnly = true;
-                    textArea.classList.toggle('fixed');
-                })
-                fixButton.innerHTML = `<i class="fas fa-wrench"></i>`;
-            } else {
-                textAreas.forEach((textArea) => {
-                    textArea.readOnly = false;
-                    textArea.classList.toggle('fixed');
-                })
-                fixButton.innerHTML = `<i class="fas fa-check"></i>`;
-            }
-            gridRow.classList.toggle('fixed');
-            fixButton.classList.toggle('fixed');
-
+function unlockButtons(){
+    var unlockButtons = document.querySelectorAll('.unlock');
+    const stringModal = 'Bạn có chắc muốn mở khóa người dùng không?';
+    const stringAlert = 'Đã mở khóa.';
+    unlockButtons.forEach((unlockButton,index) => {
+        unlockButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal,stringAlert).then((result) => {
+                if (result) {
+                    if (unlockButton) {
+                        var gridRow = event.target.closest('.grid-row');
+                        gridRow.remove();
+                        let id = gridRow.querySelector('.grid-row textarea[placeholder="Nhập id..."]').value;
+                        let index = users.findIndex(user => user.id == id);
+                        users[index].isBanned = "false";
+                    }
+                }
+            })
+        })
+    })
+}
+function unlockButtonsAllUsers(){
+    var unlockButtons = document.querySelectorAll('.unlock');
+    const stringModal = 'Bạn có chắc muốn mở khóa người dùng không?';
+    const stringAlert = 'Đã mở khóa.';
+    unlockButtons.forEach((unlockButton,index) => {
+        unlockButton.addEventListener('click', (event) => {
+            var confirm = openModal(stringModal,stringAlert).then((result) => {
+                if (result) {
+                    if (unlockButton) {
+                        var gridRow = event.target.closest('.grid-row');
+                        let id = gridRow.querySelector('.grid-row textarea[placeholder="Nhập id..."]').value;
+                        let index = users.findIndex(user => user.id == id);
+                        users[index].isBanned = "false";
+                        allUsers(listUsersBlock);
+                    }
+                }
+            })
         })
     })
 }
 
+function userFilter(){
+    const userFilter = document.getElementById('userFilter');
+    var listUsersBlock = document.querySelector('#dataUsers');
+    users.forEach(function (user) {
+        if (userFilter.value == "activeUsers") activeUsers(listUsersBlock);
+        else if (userFilter.value == "bannedUsers") bannedUsers(listUsersBlock);
+        else if (userFilter.value == "allUsers") allUsers(listUsersBlock);
+    });
+}
+
+function activeUsers(listUsersBlock){
+    listUsersBlock.innerHTML = '';
+    users.forEach(function (user) {
+        if (user.isBanned == "false"){
+            var newUser = document.createElement('div');
+            newUser.className = 'grid-row';
+            newUser.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${user.id}</textarea>
+                <textarea placeholder="Nhập tên người dùng..." readonly>${user.username}</textarea>
+                <textarea placeholder="Nhập số điện thoại..." readonly>${user.phone}</textarea>
+                <textarea placeholder="Nhập email..." readonly>${user.email}</textarea>
+                <textarea placeholder="Nhập nội dung..." readonly>${user.address}</textarea>
+                <div class="tool">
+                    <button type="button" class="fix">
+                        <i class="fas fa-wrench"></i>
+                    </button>
+                    <button type="button" class="delete">
+                        <i class="fas fa-ban"></i>
+                    </button>
+                </div>
+            `;
+            listUsersBlock.appendChild(newUser);
+        }  
+    });
+    fixButtons();
+    banButtons();
+}
+
+function allUsers(listUsersBlock){
+    listUsersBlock.innerHTML = '';
+    users.forEach(function (user) {
+        var newUser = document.createElement('div');
+        newUser.className = 'grid-row';
+        if (user.isBanned == "true"){
+            newUser.classList.add('banned');
+            newUser.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${user.id}</textarea>
+                <textarea placeholder="Nhập tên người dùng..." readonly>${user.username}</textarea>
+                <textarea placeholder="Nhập số điện thoại..." readonly>${user.phone}</textarea>
+                <textarea placeholder="Nhập email..." readonly>${user.email}</textarea>
+                <textarea placeholder="Nhập nội dung..." readonly>${user.address}</textarea>
+                <div class="tool">
+                    <button type="button" class="unlock">
+                        <i class="fas fa-unlock"></i>
+                    </button>
+                </div>
+            `;
+        } else if (user.isBanned == "false"){
+            newUser.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${user.id}</textarea>
+                <textarea placeholder="Nhập tên người dùng..." readonly>${user.username}</textarea>
+                <textarea placeholder="Nhập số điện thoại..." readonly>${user.phone}</textarea>
+                <textarea placeholder="Nhập email..." readonly>${user.email}</textarea>
+                <textarea placeholder="Nhập nội dung..." readonly>${user.address}</textarea>
+                <div class="tool">
+                    <button type="button" class="fix">
+                        <i class="fas fa-wrench"></i>
+                    </button>
+                    <button type="button" class="delete">
+                        <i class="fas fa-ban"></i>
+                    </button>
+                </div>
+            `;
+        }
+        listUsersBlock.appendChild(newUser);  
+    });
+    fixButtons();
+    banButtonsAllUsers();
+    unlockButtonsAllUsers();
+}
+
+function bannedUsers(listUsersBlock){
+    listUsersBlock.innerHTML = '';
+    users.forEach(function (user) {
+        if (user.isBanned == "true"){
+            var newUser = document.createElement('div');
+            newUser.className = 'grid-row';
+            newUser.classList.add('banned');
+            newUser.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${user.id}</textarea>
+                <textarea placeholder="Nhập tên người dùng..." readonly>${user.username}</textarea>
+                <textarea placeholder="Nhập số điện thoại..." readonly>${user.phone}</textarea>
+                <textarea placeholder="Nhập email..." readonly>${user.email}</textarea>
+                <textarea placeholder="Nhập nội dung..." readonly>${user.address}</textarea>
+                <div class="tool">
+                    <button type="button" class="unlock">
+                        <i class="fas fa-unlock"></i>
+                    </button>
+                </div>
+            `;
+            listUsersBlock.appendChild(newUser);
+        }  
+    });
+    unlockButtons();
+}
+
+start();
