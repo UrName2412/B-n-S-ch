35cb903795c5ad1e5c5f9df72e6527642d304799
NgoTruongVu05 <ngotruongvu05@gmail.com>
2025-03-17 17:47:36 +0700

    modified:   admin/asset/js/inputDataCart.js 	modified:   admin/data/JSON/donhang.json 	modified:   admin/page/donhang.html

--- admin/asset/js/inputDataCart.js
+++ admin/asset/js/inputDataCart.js
@@ -23,9 +23,9 @@ function renderCarts() {
 }
 
 
-function setStatusButtons(statusButtons,isAllCarts){
+function setStatusButtons(statusButtons, isAllCarts) {
     statusButtons.forEach((statusButton) => {
-        setStatus(statusButton.innerHTML,statusButton);
+        setStatus(statusButton.innerHTML, statusButton);
         statusButton.addEventListener('click', (event) => {
             if (statusButton.innerHTML == 'Chưa xử lí') statusButton.innerHTML = "Đã xác nhận";
             else if (statusButton.innerHTML == 'Đã xác nhận') statusButton.innerHTML = "Đã giao";
@@ -36,14 +36,14 @@ function setStatusButtons(statusButtons,isAllCarts){
             let id = gridRowCart.querySelector('.grid-row-cart textarea[placeholder="Nhập id..."]').value;
             let index = carts.findIndex(cart => cart.id == id);
             carts[index].status = statusButton.innerHTML;
-            if (isAllCarts === false){
+            if (isAllCarts === false) {
                 gridRowCart.remove();
             }
-            setStatus(statusButton.innerHTML,statusButton);
+            setStatus(statusButton.innerHTML, statusButton);
         })
     })
 
-    function setStatus(cartStatus,statusButton){
+    function setStatus(cartStatus, statusButton) {
         statusButton.className = 'status';
         if (cartStatus == 'Chưa xử lí') statusButton.classList.add('not-confirm');
         else if (cartStatus == 'Đã xác nhận') statusButton.classList.add('confirm');
@@ -53,7 +53,7 @@ function setStatusButtons(statusButtons,isAllCarts){
     }
 }
 
-function setDetailButtons(detailButtons){
+function setDetailButtons(detailButtons) {
     detailButtons.forEach(detailButton => {
         detailButton.addEventListener('click', (event) => {
             var gridRowCart = event.target.closest('.grid-row-cart');
@@ -112,8 +112,8 @@ function searchButton() {
     } else {
         listCartsBlock.innerHTML = '';
         carts.forEach(cart => {
-            for (const value in cart){
-                if (typeof(cart[value]) !== "string"){
+            for (const value in cart) {
+                if (typeof (cart[value]) !== "string") {
                     var data = String(cart[value]);
                 } else data = cart[value]
                 data = data.trim().toLowerCase();
@@ -138,12 +138,12 @@ function searchButton() {
         })
         var statusButtons = document.querySelectorAll('.status');
         const detailButtons = document.querySelectorAll('.detailButton');
-        setStatusButtons(statusButtons,true);
+        setStatusButtons(statusButtons, true);
         setDetailButtons(detailButtons);
     }
 }
 
-function allCarts(listCartsBlock){
+function allCarts(listCartsBlock) {
     listCartsBlock.innerHTML = '';
     carts.forEach(function (cart) {
         var newCart = document.createElement('div');
@@ -163,14 +163,14 @@ function allCarts(listCartsBlock){
     });
     var statusButtons = document.querySelectorAll('.status');
     const detailButtons = document.querySelectorAll('.detailButton');
-    setStatusButtons(statusButtons,true);
+    setStatusButtons(statusButtons, true);
     setDetailButtons(detailButtons);
 }
 
-function statusCarts(listCartsBlock,status){
+function statusCarts(listCartsBlock, status) {
     listCartsBlock.innerHTML = '';
     carts.forEach(function (cart) {
-        if (cart.status == status){
+        if (cart.status == status) {
             var newCart = document.createElement('div');
             newCart.className = 'grid-row-cart';
             newCart.innerHTML = `
@@ -189,29 +189,91 @@ function statusCarts(listCartsBlock,status){
     });
     var statusButtons = document.querySelectorAll('.status');
     var detailButtons = document.querySelectorAll('.detailButton');
-    setStatusButtons(statusButtons,false);
+    setStatusButtons(statusButtons, false);
     setDetailButtons(detailButtons);
 }
 
-function cartFilter(){
+function cartFilter() {
     const cartFilter = document.getElementById('cartFilter');
-    if (cartFilter.value == "allCarts") allCarts(listCartsBlock);
-    else if (cartFilter.value == "cartsNotConfirm") statusCarts(listCartsBlock,"Chưa xử lí");
-    else if (cartFilter.value == "cartsConfirm") statusCarts(listCartsBlock,"Đã xác nhận");
-    else if (cartFilter.value == "cartsDelivered") statusCarts(listCartsBlock,"Đã giao");
-    else if (cartFilter.value == "cartsCanceled") statusCarts(listCartsBlock,"Đã hủy");
+    if (cartFilter.value == "Tất cả đơn hàng") allCarts(listCartsBlock);
+    else if (cartFilter.value == "Chưa xử lí") statusCarts(listCartsBlock, "Chưa xử lí");
+    else if (cartFilter.value == "Đã xác nhận") statusCarts(listCartsBlock, "Đã xác nhận");
+    else if (cartFilter.value == "Đã giao") statusCarts(listCartsBlock, "Đã giao");
+    else if (cartFilter.value == "Đã hủy") statusCarts(listCartsBlock, "Đã hủy");
 }
 
 let filterBtn = document.getElementById('filterBtn');
 let menuFilter = document.querySelector('.menuFilter');
 filterBtn.onclick = () => {
-    if (menuFilter.classList.contains('appear')){
+    if (menuFilter.classList.contains('appear')) {
         menuFilter.style.display = 'none';
         menuFilter.classList.remove('appear');
-    }else{
+    } else {
         menuFilter.style.display = 'flex';
         menuFilter.classList.add('appear');
     }
+}
+
+
+
+function handleFilter(dateStart, dateEnd, city, district) {
+    dateStart = (dateStart == "") ? null : new Date(dateStart);
+    dateEnd = (dateEnd == "") ? null : new Date(dateEnd);
+    city = (city == "") ? null : city.trim().toLowerCase();
+    district = (district == "") ? null : district.trim().toLowerCase();
+
+    if (dateStart && isNaN(dateStart.getTime())) dateStart = null;
+    if (dateEnd && isNaN(dateEnd.getTime())) dateEnd = null;
+
+    if (dateStart && dateEnd && dateStart > dateEnd) {
+        alert("Ngày kết thúc phải lớn hơn ngày bắt đầu");
+        return;
+    }
+    var cartFilterValue = document.getElementById('cartFilter').value;
+
+    if (!dateStart && !dateEnd && !city && !district) {
+        listCartsBlock.innerHTML = '';
+        cartFilter();
+        return;
+    }
+    listCartsBlock.innerHTML = '';
+
+    for (let i = 0; i < carts.length; i++) {
+        cartTime = new Date(carts[i].date);
+        if (carts[i].status == cartFilterValue || cartFilterValue == "Tất cả đơn hàng") {
+            
+            var cityTemp = carts[i].address.split(",")[1].trim().toLowerCase();
+            var districtTemp = carts[i].address.split(",")[0].trim().toLowerCase(); 
+
+            if (city && city !== cityTemp) continue;
+            if (district && district !== districtTemp) continue;
+
+            if (dateStart && cartTime < dateStart) continue;
+            if (dateEnd && cartTime > dateEnd) continue;
+
+            var newCart = document.createElement('div');
+            newCart.className = 'grid-row-cart';
+            newCart.innerHTML = `
+                <textarea placeholder="Nhập id..." readonly>${carts[i].id}</textarea>
+                <textarea placeholder="Nhập tên người dùng..." readonly>${carts[i].username}</textarea>
+                <textarea placeholder="Nhập địa chỉ..." readonly>${carts[i].address}</textarea>
+                <textarea placeholder="Nhập số điện thoại..." readonly>${carts[i].phone}</textarea>
+                <textarea placeholder="Nhập tổng giá..." readonly>${carts[i].amount}</textarea>
+                <div class="buttonCart">
+                    <button type="button" class="status">${carts[i].status}</button>
+                    <button type="button" class="detailButton">Chi tiết đơn hàng</button>
+                </div>
+            `;
+            listCartsBlock.appendChild(newCart);
+            console.log(newCart);
+        }
+    }
+    var statusButtons = document.querySelectorAll('.status');
+    var detailButtons = document.querySelectorAll('.detailButton');
+    if (cartFilterValue == "Tất cả đơn hàng") 
+        setStatusButtons(statusButtons, true);
+    else setStatusButtons(statusButtons, false);
+    setDetailButtons(detailButtons);
 }
 
 
@@ -220,3 +282,4 @@ start();
 document.addEventListener("DOMContentLoaded", () => {
     response768('.grid-row-cart');
 });
+
