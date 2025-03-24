43bccff9ec7c0e8ddc6dd714c0a0b1f841a0fc72
UrName2412 <thienduong24122005@gmail.com>
2025-03-24 08:51:30 +0700

    modified:   index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cửa hàng sách</title>
+    <title>Cửa hàng sách Vương Hạo</title>
+    <meta name="description" content="Cửa hàng sách Vương Hạo cung cấp toàn quốc.">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- FONT AWESOME  -->
