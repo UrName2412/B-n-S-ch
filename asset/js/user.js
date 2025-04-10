e7ef0d53ae3bd10a70b55627793e4779b7a3a527
Cgtho <robinhuynh05@gmail.com>
2025-04-10 14:26:50 +0700

    update thong tin user (ch luu csdl)

--- asset/js/user.js
+++ asset/js/user.js
@@ -19,3 +19,43 @@ const loadFromsessionStorage = () => {
 };
 
 loadFromsessionStorage();
+
+let provinces = {};
+let districts = {};
+let wards = {};
+
+async function loadData() {
+    provinces = await fetch("../vender/apiAddress/province.json").then(res => res.json());
+    districts = await fetch("../vender/apiAddress/district.json").then(res => res.json());
+    wards = await fetch("../vender/apiAddress/ward.json").then(res => res.json());
+
+    let provinceSelect = document.getElementById("province");
+    provinces.forEach(province => {
+        let option = new Option(province.name, province.code);
+        provinceSelect.add(option);
+    });
+}
+
+function loadDistricts() {
+    let provinceCode = document.getElementById("province").value;
+    let districtSelect = document.getElementById("district");
+    districtSelect.innerHTML = '<option value="">Chọn quận/huyện</option>';
+
+    districts.filter(d => d.province_code == provinceCode).forEach(district => {
+        let option = new Option(district.name, district.code);
+        districtSelect.add(option);
+    });
+}
+
+function loadWards() {
+    let districtCode = document.getElementById("district").value;
+    let wardSelect = document.getElementById("ward");
+    wardSelect.innerHTML = '<option value="">Chọn phường/xã</option>';
+
+    wards.filter(w => w.district_code == districtCode).forEach(ward => {
+        let option = new Option(ward.name, ward.code);
+        wardSelect.add(option);
+    });
+}
+
+window.onload = loadData;
