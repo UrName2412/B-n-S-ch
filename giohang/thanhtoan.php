3c34b525e700ecf14c8e1951fc27cdc32bcccaa9
ori <nguyendinhhiep273@gmail.com>
2025-04-28 23:05:49 +0700

    Update thanhtoan.php

--- giohang/thanhtoan.php
+++ giohang/thanhtoan.php
@@ -304,6 +304,63 @@ if ($user1['trangThai'] == false) {
     </div>
   </section>
 
+   <!-- Footer -->
+   <footer class="text-white py-4">
+        <div class="container">
+            <div class="row pb-4 footer__bar">
+                <div class="col-md-12 d-flex justify-content-between fw-bold align-items-center footer__connect">
+                    <p>Thời gian mở cửa: <span>07h30 - 21h30 mỗi ngày</span></p>
+                    <div class="d-flex">
+                        <p>Kết nối với chúng tôi:</p>
+                        <a href="#" class="text-white ms-3">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                        <a href="#" class="text-white ms-3">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center align-items-center footer__bar">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <a href="../index.php" class="d-flex align-items-center">
+                            <img src="../Images/LogoSach.png" alt="logo" width="100" height="57">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div>
+                        <p class="mb-1">Hotline: 1900 0000</p>
+                        <p class="mb-1">Email: nhasach@gmail.com</p>
+                        <p>&copy; 2024 Công ty TNHH Nhà sách</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list list-unstyled">
+                        <li class="list-item">
+                            <a href="#" class="text-white">Tuyển dụng</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#" class="text-white">Chính sách giao hàng</a>
+                        </li>
+                        <li class="list-item">
+                            <a href="#" class="text-white">Điều khoản và điều kiện</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row footer__bar">
+                <div class="col-md-12">
+                    <ul class="list-unstyled">
+                        <li>Chi nhánh 1: 273 An Dương Vương, Phường 3, Quận 5, TP. Hồ Chí Minh</li>
+                        <li>Chi nhánh 2: 105 Bà Huyện Thanh Quan, Phường Võ Thị Sáu, Quận 3, TP. Hồ Chí Minh</li>
+                        <li>Chi nhánh 3: 4 Tôn Đức Thắng, Phường Bến Nghé, Quận 1, TP. Hồ Chí Minh</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
   <script src="../vender/js/bootstrap.bundle.min.js"></script>
 
 
